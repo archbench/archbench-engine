5adec1af0d0dfa2666b4824447e6821c7cc7a9ce
ddagos <dfromcl@yahoo.it>
2025-10-31 19:01:55 +0100

    chore: initial Spring Boot setup

---
+++ src/main/java/org/archbench/engine/ArchbenchEngineApplication.java
@@ -0,0 +1,13 @@
+package org.archbench.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchbenchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchbenchEngineApplication.class, args);
+	}
+
+}
