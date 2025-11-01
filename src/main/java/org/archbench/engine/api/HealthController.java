174e3b59144e5664a4370f315de8e908bc0294c8
ddagos <dfromcl@yahoo.it>
2025-11-01 08:50:14 +0100

    feat: add /health endpoint

---
+++ src/main/java/org/archbench/engine/api/HealthController.java
@@ -0,0 +1,13 @@
+package org.archbench.engine.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
